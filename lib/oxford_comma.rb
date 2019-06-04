a1998ea3c2ac0add544f834b49a0e40606738e57
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 22:18:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
+array = ["fiddleheads","okra","kohlrabi"]
+def oxford_comma(array)
+ if array.length == 1
+   array.join
+ elsif array.length == 2
+   array.join(" and ")
+elsif array.length >= 3
+  array[-1].insert(0, "and ")
+  array.join(", ")
+
+
+end
 end
